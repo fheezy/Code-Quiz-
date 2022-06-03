ecee457b9f52a0b2797941c8e276fcc7d77b67e2
farhan <farhanhoque764@yahoo.com>
2022-06-02 22:40:20 -0500

    modified script.js

--- JS/script.js
+++ JS/script.js
@@ -31,7 +31,7 @@ function startQuiz(){
     console.log("quiz is starting")
     startScreen.classList.add("hide")
     // need to start the timer
-    timeLeft = setInterval(clocktick,1000);
+    
     //display a question
     displayQuestion();
 }
